41ee2eb9f0f96d59689670543e384acc72fb0e3e
Tenzin Dasang <tenzindasang@MacBook-Air-3.local>
2019-10-15 14:29:20 -0700

    post fall break update

--- level1.js
+++ level1.js
@@ -1,7 +1,11 @@
 
 	var canvas = document.getElementById("myCanvas");
 	var context = canvas.getContext('2d');
+
+	var NORTH = 1, SOUTH = 2, EAST = 3; WEST = 4;
 	
+	var hero = new Hero();
+
 	let bckgrnd = new Image();
 		bckgrnd.src = "tree.jpg";
 
@@ -16,13 +20,22 @@
 
 	var doorClosed = new Image();
 		doorClosed.src = "doorClosed.png";
+
+	var healthpotion = new Image();
+		healthpotion.src = "potion.png"; 
+
+	var star = new Image();
+		star.src = "star.png";
 	
+	window.addEventListener("keydown", handleKeyDown);
+	window.addEventListener("keyup", handleKeyUp);
+
 	setInterval(loop, 33);
 
 		function handleKeyDown(event) {
 			if(event.key == 'w' && hero.jumingUp == false) {
 				hero.jumingUp = true;
-				hero.jumpingSpeed = hero.startingJumpSpeed;
+				//hero.jumpingSpeed = hero.startingJumpSpeed;
 			}
 			else if(event.key == 's') {
 				hero.movingDown = true;
@@ -35,16 +48,15 @@
 			}
 			else if(event.key == 'p' && hero.attacking == false){
 				hero.attacking = true;
-				hero.kickSound.play();
 			}
 		}
 		
 		function handleKeyUp(event) {
 			if(event.key == 'w') {
-				//hero.jumingUp = false;
+				hero.jumpingUp = false;
 			}
 			else if(event.key == 's') {
-				hero.movingDown = false;
+				hero.crouch = false;
 			}
 			else if(event.key == 'a') {
 				hero.movingLeft = false;
@@ -61,7 +73,7 @@
 
 	function loop(){
 		clearBackground();
-		/*moveAllObjects();*/
+		moveAllObjects();
 		drawAllObjects();
 	}
 
@@ -69,9 +81,13 @@
 		context.drawImage(bckgrnd, 0, 0);
 	}
 
-	/*function moveAllObjects(){
+	function moveAllObjects(){
+		hero.move();
+		/* for(var pos = 0; pos < enemies.length; pos++) {
+			enemies[pos].move();
+		} */ 
 
-	} */
+	} 
 
 	function drawAllObjects(){
 		context.drawImage(lava, 500, 665, 100, 100);
@@ -80,6 +96,8 @@
 		context.drawImage(door, 20, 588);
 		context.drawImage(doorClosed, 820, 28);
 
+		context.drawImage(star, 800, 575, 60, 60);
+
 		for(var pos = 0; pos < walls.length; pos++) {
 			walls[pos].draw();
 		}
@@ -87,10 +105,121 @@
 		for(var pos = 0; pos < coins.length; pos++) {
 			coins[pos].draw();
 		}
-
-
+		for(var pos = 0; pos < potions.length; pos++) {
+			potions[pos].draw();
+		}
+		hero.draw();
 	}
 
+	function Hero() {
+		this.x = 25;
+		this.y = 588;
+
+		this.doorOpen = new Image();
+		this.doorOpen.src = "singledoor.png";
+
+	/*MOVING SPRITES*/
+
+		/* each attack box is 89 x 62 */
+
+			/* 275 x 129 */
+		this.leftAttack = new Image();
+			this.leftAttack.src = "leftAttackSprite.png";
+		this.rightAttack = new Image();
+			this.rightAttack.src = "rightAttackSprite.png";
+		
+		/* each normal box is 54 x 54 */
+
+			/* 113 x 56 */
+		this.leftCrouch = new Image();
+			this.leftCrouch.src = "leftCrouchSprite.png";
+		this.rightCrouch = new Image();
+			this.rightCrouch.src = "rightCrouchSprite.png";
+		
+		this.leftInjury = new Image();
+			this.leftInjury.src = "leftInjurySprite.png";
+		this.rightInjury = new Image();
+			this.rightInjury.src = "rightInjurySprite.png";
+		
+			/* 227 x 56 */
+		this.leftJump = new Image;
+			this.leftJump.src = "leftJumpSprite.png";
+		this.rightJump = new Image();
+			this.rightJump.src = "rightJumpSprite.png";
+			
+			/* 455 x 56 */
+		this.leftRun = new Image();
+			this.leftRun.src = "leftRunSprite.png";
+		this.rightRun = new Image();
+			this.rightRun.src = "rightRunSprite.png";
+			
+			/* 170 x 56 */
+		this.leftRest = new Image();
+			this.leftRest.src = "leftRestSprite.png";
+		this.rightRest = new Image();
+			this.rightRest.src = "rightRestSprite.png";
+
+		this.currentSpriteSheet = this.rightRest;
+
+		this.width = 60;
+		this.height = 60;
+		this.speed = 10;
+
+		this.attacking = false;
+		this.movingRight = false; 
+		this.movingLeft = false; 
+		this.jumpingUp = false; 
+		this.falling = false;
+		this.crouch = false; 
+		this.rest = true; 
+
+		this.currentImagePosition = 0; 
+		/*NORMAL BOX*/
+			this.actualNormalSpriteWidth = 54;
+			this.actualNormalSpriteHeight = 54;
+			/* for crouch and injury */
+			this.total2ImageCount = 2;
+			/* for jump */
+			this.total4ImageCount = 4;
+			/* for rest */
+			this.total3ImageCount = 3;
+			/* for run */
+			this.total8ImageCount = 8;
+
+		/*ATTACK BOX*/
+			this.actualAttackSpriteWidth = 89;
+			this.actualAttackSpriteHeight = 62; 
+			this.totalAttackImageCount = 6;
+
+		this.move = function() {
+			var origX = this.x; 
+			var origY = this.y;
+
+			this.currentImagePosition++;
+			if(this.attacking){
+				this.currentImagePosition %= this.totalAttackImageCount;
+			}
+			else if(this.movingRight){
+				this.currentImagePosition %= this.total8ImageCount;
+				this.x += this.speed;
+			}
+			else if(this.movingLeft){
+				this.currentImagePosition %= this.total8ImageCount;
+				this.x -= this.speed;
+			}
+			
+		}
+
+		this.draw = function(){
+			var spriteX = 0;
+			var spriteY = 0;
+
+			if(this.movingLeft || movingRight){
+
+			}
+		
+		}
+	}	
 
 	function wall( x, y, w, h ) {
 		this.x = x;
@@ -137,23 +266,38 @@
 		}	
 	}
 
-	function HealthPotion( x, y ) {
-		this.status = 1; 
-		this.potionSprite = new Image();
-		this.potionSprite.src = "potion.png";
-		this.healthX = x;
-		this.healthY = y;
-		this.width = 60;
-		this.height = 50;
-		this.potionPoints = 20;
-
-		this.draw = function() {
-			if(this.status == 1) {
-				context.drawImage(this.potionSprite, this.healthX,
-					this.healthY, this.width, this.height);
+		function HealthPotion(x, y){
+			this.status = 1; 
+			this.healthX = x; 
+			this.healthY = y;
+			this.x = x;
+			this.y = y;
+			this.width = 60;
+			this.height = 50;		
+			this.potionPoints = 20; 
+			
+			
+			this.draw = function() {
+				if(this.status == 1){
+					context.drawImage(healthpotion, this.healthX, this.healthY, this.width, this.height);
+				}
 			}
 		}
 
+	function star( x, y ) {
+		this.status = 1;
+		this.starX = x;
+		this.starY = y;
+		this.starPoint = 1; 
+		this.width = 40;
+		this.height = 40; 
+
+		this.draw = function() {
+			if(this.status == 1){
+				context.drawImage(star, this.starX,
+					this.starY, this.width, this.height);
+			}
+		}
 	}
 
 
@@ -197,7 +341,7 @@
 			coins.push(new coin( 825, 475 ));
 
 		var potions = [];
-			potions.push(new HealthPotion( 100, 200 ));
+			potions.push(new HealthPotion( 25, 290 ));
 
 
 
